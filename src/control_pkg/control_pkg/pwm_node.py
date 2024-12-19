515d18ffb8b04bb2b557ec9b3cb683d4f328b9df
Chayakorn <kittikoonaim@gmail.com>
2024-12-19 22:21:48 +0700

    fix: fix accelerate and decelerate

--- src/control_pkg/control_pkg/pwm_node.py
+++ src/control_pkg/control_pkg/pwm_node.py
@@ -24,9 +24,8 @@ class PublisherSubscriberNode(Node):
         self.cur_pwm_left = 0
         self.cur_pwm_right = 0
         self.cur_dif = 0
-        self.acc = 255
-        self.dcc = 255
-        self.is_listening = False # listening state
+        self.acc = 0
+        self.dcc = 0
 
         self.timer_period = 0.1  # Publish every 0.5 second
         self.timer = self.create_timer(self.timer_period, self.timer_callback)
@@ -34,68 +33,57 @@ class PublisherSubscriberNode(Node):
         self.get_logger().info('Node initialized: Subscribing to "topic" and publishing to "output_pwm"')
 
     def listener_callback(self, msg):
-        self.is_listening = True
         
         if msg.code == 0:
             norm_angle = msg.value / 65525.0 # normalize from 0 to 1
-            if norm_angle > 0.5 and self.cur_pwm_right > 0:
-                self.cur_dif = 20 * (norm_angle - 0.5)
-            elif self.cur_pwm_left > 0:
-                self.cur_dif = 20 * (norm_angle - 0.5)
+            self.cur_dif = 100 * (norm_angle - 0.5)
         elif msg.code == 2: # accelerate
-            self.acc = msg.value
-            norm_acc = msg.value / 255.0 # normalize from 0 to 1
-            if self.cur_pwm_left < 100:
-                self.cur_pwm_left += 0.1 * (1 - norm_acc)
-            if self.cur_pwm_right < 100:
-                self.cur_pwm_right += 0.1 * (1 - norm_acc)
+            self.acc = 255 - msg.value
         elif msg.code == 5: # decelerate
-            self.dcc = msg.value
-            norm_dcc = msg.value / 255.0 # normalize from 0 to 1
-            if self.cur_pwm_left > 0:
-                self.cur_pwm_left -= 0.1 * (1 - norm_dcc)
-            if self.cur_pwm_right > 0:
-                self.cur_pwm_right -= 0.1 * (1 - norm_dcc)
+            self.dcc = 255 - msg.value
+
         #self.get_logger().info(f'Received: "{msg.code}" value: {msg.value}')
         
 
     def timer_callback(self):
         ret_pwm = PubPwm()
-        if not (self.is_listening or self.acc != 255 or self.dcc != 255): # slow down if pedal is released
-            if self.cur_pwm_right > 0:
-                self.cur_pwm_right -= 1
-            if self.cur_pwm_left > 0:
-                self.cur_pwm_left -= 1
+        if self.acc > 0:
+            if self.cur_pwm_left + 3 * (self.acc / 255.0) < 100:
+                self.cur_pwm_left += 3 * (self.acc / 255.0)
+            if self.cur_pwm_right + 3 * (self.acc / 255.0) < 100:
+                self.cur_pwm_right += 3 * (self.acc / 255.0)
+        elif self.dcc > 0:
+            if self.cur_pwm_left - 7 * (self.dcc / 255.0) > 0:
+                self.cur_pwm_left -= 7 * (self.dcc / 255.0)
+            if self.cur_pwm_right - 7 * (self.dcc / 255.0) > 0:
+                self.cur_pwm_right -= 7 * (self.dcc / 255.0)    
+        else:
+            if self.cur_pwm_left - 5 > 0:
+                self.cur_pwm_left -= 5
+            else:
+                self.cur_pwm_left = 0
+            if self.cur_pwm_right - 5 > 0:
+                self.cur_pwm_right -= 5
+            else:
+                self.cur_pwm_right = 0
 
-        if self.acc != 255 and not self.is_listening: # continue accelerate if pedal is pressed
-            hold_acc = self.acc / 255.0 # normalize from 0 to 1
-            if self.cur_pwm_left < 100:
-                self.cur_pwm_left += 3 * (1 - hold_acc)
-            if self.cur_pwm_right < 100:
-                self.cur_pwm_right += 3 * (1 - hold_acc)
-
-        if self.dcc != 255 and not self.is_listening: # continue decelerate if pedal is pressed
-            hold_dcc = self.dcc / 255.0 # normalize from 0 to 1
-            if self.cur_pwm_left > 0:
-                self.cur_pwm_left -= 10 * (1 - hold_dcc)
-            if self.cur_pwm_right > 0:
-                self.cur_pwm_right -= 10 * (1 - hold_dcc)
-
-        if self.cur_pwm_left - self.cur_dif < 0 :
-            self.cur_pwm_left = 0
-        elif self.cur_pwm_left - self.cur_dif > 100 :
-            self.cur_pwm_left -= self.cur_pwm_left + self.cur_dif - 100
-        if self.cur_pwm_right + self.cur_dif < 0 :
-            self.cur_pwm_right = 0
-        elif self.cur_pwm_right + self.cur_dif > 100 :
-            self.cur_pwm_right -= self.cur_pwm_right + self.cur_dif - 100
-        ret_pwm.left_pwm = round(self.cur_pwm_left - self.cur_dif)
-        ret_pwm.right_pwm = round(self.cur_pwm_right + self.cur_dif)
-
-        self.is_listening = False        
+        ret_pwm.right_pwm = round(self.cur_pwm_right)
+        ret_pwm.left_pwm = round(self.cur_pwm_left)
+        if self.cur_dif > 0: #turn right
+            ret_pwm.right_pwm = round(self.cur_pwm_right - self.cur_dif)
+        elif self.cur_dif < 0: #turn left
+            ret_pwm.left_pwm = round(self.cur_pwm_left + self.cur_dif)
+        
+        if ret_pwm.right_pwm < 0:
+            ret_pwm.right_pwm = 0
+        if ret_pwm.left_pwm < 0:
+            ret_pwm.left_pwm = 0
         # Publish the status
         self.publisher_.publish(ret_pwm)
         self.get_logger().info(f'Publishing: Left "{ret_pwm.left_pwm}" , Right "{ret_pwm.right_pwm}"')
+        self.get_logger().info(f'Logging: acc "{self.acc}" , dcc "{self.dcc}"')
+        self.get_logger().info(f'Logging: right "{self.cur_pwm_right}" , left "{self.cur_pwm_left}"')
+        self.get_logger().info(f'Logging: diff "{self.cur_dif}"')
 
 def main(args=None):
     rclpy.init(args=args)
