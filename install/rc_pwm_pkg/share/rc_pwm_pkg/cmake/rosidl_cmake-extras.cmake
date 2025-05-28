fbb61b1ae979a1066fb831c28571942320dff169
Chayakorn <kittikoonaim@gmail.com>
2025-05-28 14:03:40 +0700

    remove install

--- install/rc_pwm_pkg/share/rc_pwm_pkg/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(rc_pwm_pkg_IDL_FILES "msg/Pwm.idl;msg/PubPwm.idl")
-set(rc_pwm_pkg_INTERFACE_FILES "msg/Pwm.msg;msg/PubPwm.msg")
